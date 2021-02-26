20b4b543732fd0eaa45c540ffca87b51a8a8ef14
mbrunoviegas <mbrunoviegas@gmail.com>
2021-02-26 15:33:31 -0300

    Criando modulo de survey

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,10 +1,12 @@
 import { Request, Response, Router } from 'express';
-
+import { UserService } from '../services/user.service';
 export class UserController {
   public routes: Router = Router();
+  private userSerivce: UserService;
 
   constructor() {
-    this.routes.post('/', this.create)
+    this.routes.post('/', this.create);
+    this.userSerivce = new UserService();
   }
 
   private async create(request: Request, response: Response) {}
